96b74bed68f81ca3817e6cedfa78146070712edf
Ayman Patel <ayman.patel97@outlook.com>
2024-12-05 10:08:03 +0530

    feat: add cbackground-lines and floating-dock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,77 @@
+import { BackgroundLines } from "@/components/ui/background-lines";
+import { FloatingDock } from "@/components/ui/floating-dock";
+import { IconBrandX, IconFaceMask, IconGift, IconHome } from "@tabler/icons-react";
 import Image from "next/image";
-import { BentoGridDemo } from "./BentroGridDemo";
-import Hi from "./dashboard/page";
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <BentoGridDemo/>
-        <Hi/>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
+  const links = [
+    {
+      title: "Home",
+      icon: (
+        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "dashboard",
+    },
+ 
+    {
+      title: "Products",
+      icon: (
+        <IconFaceMask className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Components",
+      icon: (
+        <IconGift className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Aceternity UI",
+      icon: (
+        <Image
             aria-hidden
             src="/file.svg"
             alt="File icon"
             width={16}
             height={16}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+      ),
+      href: "#",
+    },
+    {
+      title: "Twitter",
+      icon: (
+        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+  ];
+  return (
+    // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start text-elliot">
+        <BackgroundLines className="flex items-center justify-center w-full flex-col px-4 bg-elliot">
+        <h2 className="text-alderson bg-clip-text text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+          Sanjana Airlines, <br /> Ayman 
+        </h2>
+        <p className="text-banks max-w-xl mx-auto text-sm md:text-lg dark:text-neutral-400 text-center">
+          Get the best advices from our experts, including expert artists,
+          painters, marathon enthusiasts and RDX, totally free.
+        </p>
+      </BackgroundLines>
+      {/* <h1 className="text-ayman"></h1> */}
+
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+          <FloatingDock
+            mobileClassName="translate-y-20" // only for demo, remove for production
+            items={links}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
       </footer>
-    </div>
+      </>
+    // </div>
   );
 }
--- src/app/worldmapdemo/page.tsx
+++ src/app/worldmapdemo/page.tsx
@@ -1,8 +1,9 @@
 "use client";
-import WorldMap from "@/components/ui/world-map";
+
+import { WorldMap } from "@/components/ui/world-map";
 import { motion } from "motion/react";
 
-export function WorldMapDemo() {
+export default function WorldMapDemo() {
   return (
     <div className=" py-40 dark:bg-black bg-white w-full">
       <div className="max-w-7xl mx-auto text-center">
@@ -29,36 +30,30 @@ export function WorldMapDemo() {
         </p>
       </div>
       <WorldMap
+
         dots={[
+                /*
+
+      Surat, India: Latitude: 21.1702° N, Longitude: 72.8311° E
+Vadodara, India: Latitude: 22.3072° N, Longitude: 73.1812° E
+Luton, UK: Latitude: 51.8787° N, Longitude: 0.4200° W
+Dammam, Saudi Arabia: Latitude: 26.3927° N, Longitude: 49.9777° E
+Khobar, Saudi Arabia: Latitude: 26.2172° N, Longitude: 50.1971° E
+Bombay (Mumbai), India: Latitude: 19.0760° N, Longitude: 72.8777° E
+26.4207° N, 50.0888° E
+
+      */
           {
-            start: {
-              lat: 64.2008,
-              lng: -149.4937,
-            }, // Alaska (Fairbanks)
-            end: {
-              lat: 34.0522,
-              lng: -118.2437,
-            }, // Los Angeles
+            start: { lat: 21.1702, lng: 72.8311 }, // Surat
+            end: { lat: 26.4207, lng: 50.0888 }, // Dammam
+          },   
+          {
+            start: { lat: 51.8787, lng: 0.4200 }, // Luton
+            end: { lat: 22.3072, lng: 73.1812 }, // Vadodara
           },
           {
-            start: { lat: 64.2008, lng: -149.4937 }, // Alaska (Fairbanks)
-            end: { lat: -15.7975, lng: -47.8919 }, // Brazil (Brasília)
-          },
-          {
-            start: { lat: -15.7975, lng: -47.8919 }, // Brazil (Brasília)
-            end: { lat: 38.7223, lng: -9.1393 }, // Lisbon
-          },
-          {
-            start: { lat: 51.5074, lng: -0.1278 }, // London
-            end: { lat: 28.6139, lng: 77.209 }, // New Delhi
-          },
-          {
-            start: { lat: 28.6139, lng: 77.209 }, // New Delhi
-            end: { lat: 43.1332, lng: 131.9113 }, // Vladivostok
-          },
-          {
-            start: { lat: 28.6139, lng: 77.209 }, // New Delhi
-            end: { lat: -1.2921, lng: 36.8219 }, // Nairobi
+            start: { lat: 21.1702, lng: 72.8311 }, // Surat
+            end: { lat: 19.0760, lng: 72.8777 }, // Bombay
           },
         ]}
       />
